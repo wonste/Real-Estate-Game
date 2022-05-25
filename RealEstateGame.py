a6d1f7b1ef99002144468393224c136a529e032f
stellawong <stllwong@gmail.com>
2022-05-25 12:50:20 -0700

    Description filled out. Description could be reworded another time.

--- RealEstateGame.py
+++ RealEstateGame.py
@@ -1,5 +1,9 @@
 # Author: Stella Wong
 # GitHub username: wonste
 # Date: 06/03/22
-# Description:
+# Description: The program is a very simplified version of Monopoly. The program will only allow for players to roll a
+# single die (from 1 - 6) and moving across the board in a circular motion. There is no Jail but there are properties to
+# be bought and money to be received for passing GO each time.
+
+
 
