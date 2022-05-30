f58955730b3600634b9a81766cf927b599648d76
stellawong <stllwong@gmail.com>
2022-05-29 17:03:30 -0700

    Draft 1? Need to figure out how to add to the dictionaries.

--- RealEstateGame.py
+++ RealEstateGame.py
@@ -5,5 +5,19 @@
 # single die (from 1 - 6) and moving across the board in a circular motion. There is no Jail but there are properties to
 # be bought and money to be received for passing GO each time.
 
+class RealEstateGame:
+    """
+    Represents the game
+    """
+    def __init__(self):
+        self._board_spaces = {}
+        self._players = {}
 
 
+    def create_spaces(self, money, ):
+        """
+
+        """
+        if index == 0:
+            # name this GO and space cannot be bought
+        if
