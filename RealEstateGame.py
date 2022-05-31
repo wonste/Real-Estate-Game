e44cb64db444b740d4d93fe9cc443c1f21cb0b07
Stella Wong <wongstel@oregonstate.edu>
2022-05-30 17:28:53 -0700

    get_player_account_balance method works along with the rest. Tested and confirmed to be working via PythonTutor.

--- RealEstateGame.py
+++ RealEstateGame.py
@@ -5,23 +5,50 @@
 # single die (from 1 - 6) and moving across the board in a circular motion. There is no Jail but there are properties to
 # be bought and money to be received for passing GO each time.
 
+class Player:
+
+    def __init__(self, player_name, account_balance):
+        self._player_name = player_name
+
+        self._owned_properties = []
+
+    def get_player_name(self):
+        return self._player_name
+
+
 class RealEstateGame:
     """
     Represents the game
     """
     def __init__(self):
         self._board_spaces = {}
-        self._board_rent = {}
         self._players = {}
 
-    def create_spaces(self, money, board_rent):
+    def create_spaces(self, money, rent_array):
         """
         Creates a total 25 board spaces for the Real Estate Game
         """
-        spaces = 24
-        self._board_rent[0] =
-        for index in range(spaces):
-            self._board_rent[spaces] = rent[index]
+        # initiate counter for rent_array
+        value = 0
 
+        # iterate through the total amount of board spaces
+        for index in range(0, 25):
+            # establish go
+            if index == 0:
+                self._board_spaces[index] = money
 
+            if index > 0:
+                self._board_spaces[index] = rent_array[value]
+                value += 1
 
+    def create_player(self, player_name, account_balance):
+        """
+        Create player profiles
+        """
+        self._players[player_name] = account_balance
+
+    def get_player_account_balance(self, player_name):
+
+        for name in self._players:
+            if player_name == name:
+                return self._players[player_name]
